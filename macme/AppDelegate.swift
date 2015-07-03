f018e4c0bd9ba88f43d6ac19d64b343929fcf437
Vincent Pacelli <vspacelli@gmail.com>
2015-07-03 17:55:38 -0400

    Initial Commit

---
+++ macme/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  macme
+//
+//  Created by Vincent Pacelli on 7/3/15.
+//  Copyright (c) 2015 tnecniv. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ macme/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  macme
+//
+//  Created by Vincent Pacelli on 7/3/15.
+//  Copyright (c) 2015 tnecniv. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
